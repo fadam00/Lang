68eb342557eb5cb2284dd6aceb5df0c79b62460d
Frank Adames <frankv.adames@outlook.com>
2016-10-24 15:49:07 -0400

    Added resources for users

--- Project/config/routes.rb
+++ Project/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
 root 'static_pages#home'
 
@@ -8,6 +7,8 @@ get '/about', to: "static_pages#about"
 get '/contact', to:"static_pages#contact"
 
 get '/signup', to: 'users#new'
+
+resources :users
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
