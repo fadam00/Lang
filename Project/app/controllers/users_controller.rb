c06598c25f1fba62259f957547d11ff9397975d5
Frank Adames <frankv.adames@outlook.com>
2016-10-24 21:03:15 -0400

    You can now see requests

--- Project/app/controllers/users_controller.rb
+++ Project/app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  	@requests = @user.requests.paginate(page: params[:page])
   end
 
  def create
--- Project/db/seeds.rb
+++ Project/db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -15,10 +16,18 @@ User.create!(name:  "Example User",
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@mail.com"
   password = "password"
   User.create!(name:  name,
                email: email,
                password:              password,
                password_confirmation: password)
+end
+
+#REQUESTS
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.requests.create!(content: content) }
 end
