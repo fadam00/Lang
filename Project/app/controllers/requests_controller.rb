2a266ca7461e472c0aa489ee7bb4754f48ccf364
Frank Adames <frankv.adames@outlook.com>
2016-10-24 21:09:14 -0400

    Modified home view to include requests

--- Project/app/controllers/requests_controller.rb
+++ Project/app/controllers/requests_controller.rb
@@ -2,9 +2,23 @@ class RequestsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
 
 	def create
+    @request = current_user.request.build(request_params)
+    if @request.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+    	render 'static_pages/home'
 	end
 
 	def destroy
 	end
-	
+
 end
+
+	private
+
+	def request_params
+		params.require(:request).permit(:content)
+	end
+end
+
