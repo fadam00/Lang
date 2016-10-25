1bf02a2817cd7480cf7f2663912181c8055f410f
Frank Adames <frankv.adames@outlook.com>
2016-10-24 20:34:22 -0400

    You can now delete users as an admin

--- Project/app/controllers/users_controller.rb
+++ Project/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :logged_in_user, only: [:edit, :update, :show, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -37,6 +37,12 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "User deleted"
+  	redirect_to users_url
   end
 
   private
