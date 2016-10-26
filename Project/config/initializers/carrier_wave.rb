0211a67ed6f6bcdfaea4df981c357e209dd80bf9
Frank Adames <frankv.adames@outlook.com>
2016-10-26 12:56:42 -0400

    Modifications

--- Project/config/initializers/carrier_wave.rb
+++ Project/config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAI36CS7QWXDKLDKOQ'],
-      :aws_secret_access_key => ENV['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']
-    }
-    config.fog_directory     =  ENV['languploads']
-  end
-end
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
+#     config.fog_credentials = {
+#       # Configuration for Amazon S3
+#       :provider              => 'AWS',
+#       :aws_access_key_id     => ENV['aaa'],
+#       :aws_secret_access_key => ENV['bbbb']
+#     }
+#     config.fog_directory     =  ENV['languploads']
+#   end
+# end
