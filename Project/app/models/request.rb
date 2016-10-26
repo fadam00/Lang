944f58cb914f2d55a6c612b36f7b0a05b0ac1d77
Frank Adames <frankv.adames@outlook.com>
2016-10-26 14:01:25 -0400

    Added validations to request and user models

--- Project/app/models/request.rb
+++ Project/app/models/request.rb
@@ -5,4 +5,6 @@ class Request < ApplicationRecord
   mount_uploader :attachment, AttachmentUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
+  validates :language_from, presence: true
+  validates :laguage_to, presence: true
 end
