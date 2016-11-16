b37753f94f006c477cf6669a10bc9c4def22d6ab
Frank V. Adames <frankv.adames@outlook.com>
2016-11-16 13:00:36 -0500

    Code changes and UI fixes

--- Project/app/controllers/application_controller.rb
+++ Project/app/controllers/application_controller.rb
@@ -1,33 +1,36 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include SessionsHelper
+protect_from_forgery with: :exception
 
-  rescue_from ActiveRecord::RecordNotFound do
-  flash[:warning] = 'Resource not found.'
-  redirect_back_or root_path
+include SessionsHelper
+rescue_from ActiveRecord::RecordNotFound do
+flash[:warning] = 'Resource not found.'
+redirect_back_or root_path
 end
 
 def redirect_back_or(path)
-  redirect_to request.referer || path
+redirect_to request.referer || path
 end
 
-
 helper_method :mailbox, :conversation
-  private
 
-      def logged_in_user
-    	unless logged_in?
-    		store_location
-    		flash[:danger] = "Please log in."
-    		redirect_to login_url
-    	end
-    end
 
-      def conversation
-    @conversation ||= mailbox.conversations.find(params[:id])
+private
+
+
+def logged_in_user
+  unless logged_in?
+  store_location
+  flash[:danger] = "Please log in."
+  redirect_to login_url
   end
+end
 
-   def mailbox
-    @mailbox ||= current_user.mailbox
-  end
+def conversation
+  @conversation ||= mailbox.conversations.find(params[:id])
+end
+
+def mailbox
+  @mailbox ||= current_user.mailbox
+end
+
 end
--- Project/app/models/user.rb
+++ Project/app/models/user.rb
@@ -1,58 +1,51 @@
 class User < ApplicationRecord
-	has_many :requests, dependent: :destroy
-	has_many :active_relationships, class_name: "Relationship",
-									foreign_key: "watcher_id",
-									dependent: 	:destroy
-	  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "watched_id",
-                                   dependent:   :destroy
+has_many :requests, dependent: :destroy
+has_many :active_relationships, class_name: "Relationship",
+foreign_key: "watcher_id",
+dependent: 	:destroy
+has_many :passive_relationships, class_name:  "Relationship",
+foreign_key: "watched_id",
+dependent:   :destroy
+has_many :watching, through: :active_relationships, source: :watched
+has_many :watchers, through: :passive_relationships
 
-	has_many :watching, through: :active_relationships, source: :watched 
-	 has_many :watchers, through: :passive_relationships
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+before_save {email.downcase!}
+validates :name, presence: true, length: {maximum: 50}
+validates :email, presence: true, length: {maximum: 255},
+format: {with: VALID_EMAIL_REGEX},
+uniqueness: {case_sensitive: false}
 
-	
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	before_save {email.downcase!}
-	validates :name, presence: true, length: {maximum: 50}
-	validates :email, presence: true, length: {maximum: 255},
-			 format: {with: VALID_EMAIL_REGEX},
-			 uniqueness: {case_sensitive: false}
-	
-		has_secure_password
-		validates :password, presence: true, length: {minimum: 6}
-		
+has_secure_password
+validates :password, presence: true, length: {minimum: 6}
 
-		acts_as_messageable
+acts_as_messageable
 
-
-
-	def mailboxer_email(object)
-		self.email
-	end
-
-	def mailboxer_name
-		self.name
-	end
-
-	def feed
-    watching_ids = "SELECT watched_id FROM relationships
-                     WHERE  watcher_id = :user_id"
-    Request.where("user_id IN (#{watching_ids})
-                     OR user_id = :user_id", user_id: id)
-	end
-
-	def watch(other_user)
-		active_relationships.create(watched_id: other_user.id)
-	end
-
-	def unwatch(other_user)
-		active_relationships.find_by(watched_id: other_user.id).destroy
-	end
-
-	def watching?(other_user)
-		watching.include?(other_user)
-	end
+def mailboxer_email(object)
+	self.email
 end
 
+def mailboxer_name
+	self.name
+end
 
+def feed
+	watching_ids = "SELECT watched_id FROM relationships
+	WHERE  watcher_id = :user_id"
+	Request.where("user_id IN (#{watching_ids})
+	OR user_id = :user_id", user_id: id)
+end
 
+def watch(other_user)
+	active_relationships.create(watched_id: other_user.id)
+end
+
+def unwatch(other_user)
+	active_relationships.find_by(watched_id: other_user.id).destroy
+end
+
+def watching?(other_user)
+	watching.include?(other_user)
+end
+
+end
